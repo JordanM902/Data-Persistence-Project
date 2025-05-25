b5273d6bdbcd1633ca5776b1b163f976f67b72d6
JordanM902 <89055518+JordanM902@users.noreply.github.com>
2025-05-25 04:37:24 -0300

    added data persistance
    added data persistance to a high score and player name. setup to save, load, & display all text properly.
    
    Need to add a new variable for the high score record, at the moment the highscore name will just set to the current name

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,33 +13,87 @@ using UnityEditor;
 [DefaultExecutionOrder(1000)]
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string playerName;
+    public int highScore;
+    public TMP_InputField inputField; // Change to InputField
+    public TextMeshProUGUI welcomeText;
+    public TextMeshProUGUI highScoreText; // Change to TextMeshProUGUI
+
+    void Awake()
     {
-        
+        LoadData();
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            welcomeText.text = "Welcome " + playerName + "!";
+            highScoreText.text = "High Score: " + highScore;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        inputField.onEndEdit.AddListener(OnInputEndEdit);
+    }
+
+    void OnInputEndEdit(string input)
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SetPlayerName();
+        }
+    }
+
+    public void SetPlayerName()
+    {
+        playerName = inputField.text;
+        welcomeText.text = "Welcome " + playerName + "!";
+        SaveGame();
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
     }
+
     public void Exit()
     {
-
+        SaveGame();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit();
 #endif
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int highScore;
+    }
+
+    public void SaveGame()
+    {
+        SaveData data = new SaveData();
+        data.playerName = playerName;
+        data.highScore = highScore;
+        string json = JsonUtility.ToJson(data);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (System.IO.File.Exists(path))
+        {
+            string json = System.IO.File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            playerName = data.playerName;
+            highScore = data.highScore;
+        }
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,23 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text HighScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
-    private int m_Points;
-    
+    public int m_Points;
     private bool m_GameOver = false;
 
-    
-    // Start is called before the first frame update
+    private GameManager gameManager;
+
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -35,6 +37,11 @@ public class MainManager : MonoBehaviour
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
             }
+        }
+
+        if (gameManager != null)
+        {
+            HighScoreText.text = $"High Score : {gameManager.highScore} ({gameManager.playerName})";
         }
     }
 
@@ -66,6 +73,12 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+        if (gameManager != null && m_Points > gameManager.highScore)
+        {
+            gameManager.highScore = m_Points;
+            gameManager.SaveGame();
+            HighScoreText.text = $"High Score : {gameManager.highScore} ({gameManager.playerName})";
+        }
     }
 
     public void GameOver()
